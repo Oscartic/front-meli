51f8bc5b5c3d31c358f9b30fb41571e36a147a2d
oscartic <orballesteros@falabella.cl>
2022-05-26 21:34:42 -0400

    create Breadcrumb component

---
+++ components/ui/Breadcrumb.js
@@ -0,0 +1,9 @@
+const Breadcrumb = () => {
+    return (
+        <section className='breadcrumb'>
+            <span>Computación > Notebooks y Accesorios </span>
+        </section>
+    );
+}
+ 
+export default Breadcrumb;
