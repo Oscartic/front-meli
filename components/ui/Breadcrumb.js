a63898de760f27bc63e0872fae086b52726798e3
oscartic <orballesteros@falabella.cl>
2022-05-27 12:45:13 -0400

    add makeBreadcrumb

--- components/ui/Breadcrumb.js
+++ components/ui/Breadcrumb.js
@@ -1,7 +1,14 @@
-const Breadcrumb = () => {
+const Breadcrumb = ({ categories }) => {
+    
+    console.log(categories)
+    const makeBreadCrumb = () => {
+        if(categories.length <= 0) return "Categoría nueva"
+        return categories.toString().replace(/,/g, " > ");;
+    }
+
     return (
         <section className='breadcrumb'>
-            <span>Computación > Notebooks y Accesorios </span>
+            <span> {makeBreadCrumb()} </span>
         </section>
     );
 }
